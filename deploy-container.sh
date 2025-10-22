64e80dfda3eaa15d512c85fcb29350b71e6efda1
Robin Slot <robin@rslot.nl>
2025-10-22 23:14:15 +0200

    delete old manifest

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,4 +1,5 @@
 set -e
-podman manifest create -a ssx-placeholderserver
+! podman manifest exists ssx-placeholderserver || podman manifest rm ssx-placeholderserver
+podman manifest create ssx-placeholderserver
 podman build --platform=linux/arm64,linux/amd64 --manifest ssx-placeholderserver .
 podman manifest push --all ssx-placeholderserver docker://docker.io/derkades/ssx-placeholderserver
